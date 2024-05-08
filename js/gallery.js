e25c1f9d684ca6d0d6f135700688c309f677d0b7
raplechner <89600881+raplechner@users.noreply.github.com>
2024-05-08 21:23:52 +0200

    Update gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -6,15 +6,21 @@
 function showRandomImageAtStart() {
     // TODO: Select all 6 links (<a>) in the thumbnail section. They contain the URLs to the full images.
     let allLinks = document.querySelectorAll(".card-link");
+    console.log("allLinks:", allLinks)
     // TODO: Select a random entry out of these 6.
     let randomEntryNumber = Math.floor(Math.random() * allLinks.length);
     let randomLink = allLinks[randomEntryNumber];
+    console.log("random Link:", randomLink)
     // TODO: Implement switchFullImage() below.
     // Implemented
     // TODO: Call switchFullImage() with the URL of the random image and the alt attribute of the thumbnail (it contains the description).
+    console.log("randomLink:", randomLink);
     let randomImage = randomLink.querySelector("img");
-    let randomImageUrl = randomImage.getAttribute("src");
+    console.log("randomImage:", randomImage);
+    let randomImageUrl = randomLink.getAttribute("href");
     let randomImageAlt = randomImage.getAttribute("alt");
+    console.log("randomImageUrl:", randomImageUrl);
+    console.log("randomImageAlt:", randomImageAlt);
     switchFullImage(randomImageUrl, randomImageAlt);
     // TODO: Set a background color (classes .bg-dark and .text-white) to the card-body of your random image (hint: it's the sibling element of your link).
     let cardBody = randomLink.nextElementSibling;
@@ -36,15 +42,17 @@ function prepareLinks() {
         link.addEventListener("click", function (event) {
             // TODO: The callback of the listener should do the following things:
             //  - Remove the .bg-dark and .text-white classes from the card where it's currently set.
-            let cardBodiesActive = document.querySelectorAll(".bg-dark");
+            let cardBodiesActive = document.querySelectorAll("a+.bg-dark");
             cardBodiesActive.forEach(function(cardBody) {
                 cardBody.classList.remove("bg-dark", "text-white");
-            });            //  - Add both classes again to the card where the click happened (hint: "this" contains the very <a> element, where the click happened).
+            });
+            //  - Add both classes again to the card where the click happened (hint: "this" contains the very <a> element, where the click happened).
             let cardBody = this.nextElementSibling;
             cardBody.classList.add("bg-dark", "text-white");
             //  - Call switchFullImage() with the URL clicked link and the alt attribute of the thumbnail.
-            let ImageUrl = this.getAttribute("src");
-            let ImageAlt = this.getAttribute("alt");
+            let imageChild = this.children[0];
+            let ImageUrl = this.getAttribute("href");
+            let ImageAlt = imageChild.getAttribute("alt");
             switchFullImage(ImageUrl, ImageAlt);
             //  - Implement and then call loadNotes() with the key for the current image (hint: the full image's URL makes an easy and unique key).
             loadNotes(ImageUrl);
@@ -63,7 +71,7 @@ function storeNotes() {
         // TODO: When the notes field loses focus, store the notes for the current image in the local storage.
         let notes = noteField.textContent.trim();
         // TODO: Choose an appropriate key (hint: the full image's URL makes an easy and unique key).
-        let key = document.getElementById("fullImage").getAttribute("src");
+        let key = document.querySelector("#fullImage img").getAttribute("src");
 
         // TODO: If the notes field is empty, remove the local storage entry.
         if (notes === "") {
@@ -83,6 +91,9 @@ function storeNotes() {
  * @param {string} imageDescription The image's description (used for the alt attribute and the figure's caption).
  */
 function switchFullImage(imageUrl, imageDescription) {
+    console.log("Image URL:", imageUrl);
+    console.log("Image Description:", imageDescription);
+
     // TODO: Get the <img> element for the full image. Select it by its class or tag name.
     let fullImage = document.querySelector("#fullImage img");
     // TODO: Set its src and alt attributes with the values from the parameters (imageUrl, imageDescription).
